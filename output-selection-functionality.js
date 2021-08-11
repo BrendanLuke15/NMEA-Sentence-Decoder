8fc251e48dbb5d79ea95cf004df63b89c7062d7c
Brendan Luke <31905278+BrendanLuke15@users.noreply.github.com>
2021-08-11 13:10:52 -0400

    building CSV or GPX selection functionality

---
+++ output-selection-functionality.js
@@ -0,0 +1,13 @@
+/*
+By: Brendan Luke
+
+Date: August 11, 2021
+
+Purpose: this Javascript file contatins the functionality for selecting the output type (CSV or GPX).
+*/
+
+
+// Output Selection
+function outputType() {
+
+}
