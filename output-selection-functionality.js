d268b630188935ad1a2b8323dfe43ab723c03520
Brendan Luke <31905278+BrendanLuke15@users.noreply.github.com>
2021-08-12 09:52:34 -0400

    setup page for KML

--- output-selection-functionality.js
+++ output-selection-functionality.js
@@ -1,27 +1,39 @@
 /*
 By: Brendan Luke
 
-Date: August 11, 2021
+Date: August 12, 2021
 
-Purpose: this Javascript file contatins the functionality for selecting the output type (CSV or GPX).
+Purpose: this Javascript file contatins the functionality for selecting the output type (CSV, GPX, or KML).
 */
 
 
 // Output Selection functions
 function selectCSV() {
-    // set logic bit
-    outputFlag = true;
+    // set logic index
+    outputFlag = 1;
 
     // change 'button' stylings
     document.getElementById('CSV').className = 'header-select-icon-active';
     document.getElementById('GPX').className = 'header-select-icon';
+    document.getElementById('KML').className = 'header-select-icon';
 }
 
 function selectGPX() {
-    // set logic bit
-    outputFlag = false;
+    // set logic index
+    outputFlag = 2;
 
     // change 'button' stylings
     document.getElementById('GPX').className = 'header-select-icon-active';
     document.getElementById('CSV').className = 'header-select-icon';
+    document.getElementById('KML').className = 'header-select-icon';
+}
+
+function selectKML() {
+    // set logic index
+    outputFlag = 3;
+
+    // change 'button' stylings
+    document.getElementById('KML').className = 'header-select-icon-active';
+    document.getElementById('CSV').className = 'header-select-icon';
+    document.getElementById('GPX').className = 'header-select-icon';
 }
